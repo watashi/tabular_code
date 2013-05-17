0cecbe04ad78e1b07bf926e41202aa1fbdc4f318
Zejun Wu <zejun.wu@gmail.com>
2013-05-17 18:53:12 +0800

    Refactor base

--- lib/tabular_code/base.rb
+++ lib/tabular_code/base.rb
@@ -6,16 +6,40 @@ module TabularCode
   end
 
   module Base
+    def from_char(c)
+      c = c.encode(@@encoding).ord rescue (return nil)
+      x = c / 0x100 - 0xA0
+      y = c % 0x100 - 0xA0
+      valid?(x, y) ? x * 100 + y : nil
+    end
+
+    def to_char(c)
+      x = c / 100
+      y = c % 100
+      c = valid?(x, y) ? (0xA0 + x) * 0x100 + (0xA0 + y) : nil
+      c.chr(@@encoding).encode(Encoding::UTF_8) rescue (return nil)
+    end
+
     def from_str(s, options = {:replace => nil})
-      ret = s.chars.map{|i| self.from_char(i)}
+      ret = s.chars.map{|i| from_char(i)}
       raise ConversionError if !options.has_key?(:replace) && ret.include?(nil)
       ret.map{|i| i.nil? ? options[:replace] : i}
     end
 
     def to_str(s, options = {:replace => nil})
-      ret = s.map{|i| self.to_char(i)}
+      ret = s.map{|i| to_char(i)}
       raise ConversionError if !options.has_key?(:replace) && ret.include?(nil)
       ret.map{|i| i.nil? ? options[:replace] : i} * ''
+    end
+
+    protected
+    def encoding=(encoding)
+      @@encoding = encoding
+    end
+
+    private
+    def valid?(x, y)
+      (1..94).include?(x) && (1..94).include?(y)
     end
   end
 end
--- lib/tabular_code/quwei.rb
+++ lib/tabular_code/quwei.rb
@@ -4,22 +4,6 @@ module TabularCode
   module Quwei
     extend Base
 
-    def self.valid?(hi, lo)
-      (1..87).include?(hi) && (1..94).include?(lo)
-    end
-
-    def self.from_char(c)
-      c = c.encode(Encoding::EUC_CN).ord rescue (return nil)
-      hi = (c >> 8) - 0xA0
-      lo = (c & 0xff) - 0xA0
-      valid?(hi, lo) ? hi * 100 + lo : nil
-    end
-
-    def self.to_char(c)
-      hi = 0xA0 + (c / 100)
-      lo = 0xA0 + (c % 100)
-      c = valid?(c / 100, c % 100) ? (hi << 8) ^ lo : nil
-      c.chr(Encoding::EUC_CN).encode(Encoding::UTF_8) rescue (return nil)
-    end
+    self.encoding = Encoding::EUC_CN
   end
 end
